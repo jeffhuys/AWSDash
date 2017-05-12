c8fbffe69551aed0f08456edd29b47d10cdd0da6
Jeff Huijsmans <jeff@bleeve.nl>
2017-05-12 16:02:01 +0200

    Change eslintrc to js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,10 @@
-{
+module.exports = {
     "env": {
         "es6": true,
         "node": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": 2017,
         "sourceType": "module"
     },
     "rules": {
@@ -26,4 +25,4 @@
             "never"
         ]
     }
-}
+};
